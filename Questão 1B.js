91d869945c24af8bcaae8035370def30a45ee849
vtspp <49285474+vtspp@users.noreply.github.com>
2019-10-24 00:46:41 -0300

    Rename questao _B.js to Questão 1B.js

--- Questão 1B.js
+++ Questão 1B.js
@@ -12,29 +12,29 @@ var str = prompt("digite 6 caracteres entre (), {}, []")
             
             else if(str.charAt(i) == pilha[1])
                  if(pilha.isEmptyObject)
-                document.write('N„o est· balanceado')
+                document.write('N√£o est√° balanceado')
                  if(pilha.pop() != pilha[0])
-                document.write('N„o est· balanceado')
+                document.write('N√£o est√° balanceado')
             else if{
-                document.write("Est· Balanceado")
+                document.write("Est√° Balanceado")
             }//fim da pilha[1]*/
             
             else if(str.charAt(i) == pilha[3])
                  if(pilha.isEmptyObject)
-                document.write('N„o est· balanceado')
+                document.write('N√£o est√° balanceado')
                  if(pilha.pop() != pilha[2])
-                document.write('N„o est· balanceado')
+                document.write('N√£o est√° balanceado')
             else if{
-                document.write("Est· Balanceado")
+                document.write("Est√° Balanceado")
             }//fim da pilha[2]*/
             
             else if(str.charAt(i) == pilha[5])
                  if(pilha.isEmptyObject)
-                document.write('N„o est· balanceado')
+                document.write('N√£o est√° balanceado')
                  if(pilha.pop() != pilha[4])
-                document.write('N„o est· balanceado')
+                document.write('N√£o est√° balanceado')
             else {
-                document.write("Est· Balanceado")
+                document.write("Est√° Balanceado")
             }//fim da pilha[4]*/
             
         }
